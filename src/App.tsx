b48766a3daa1afc4973b9b80d063d27951dbef0d
sayjeyhi <sayjeyhi@gmail.com>
2021-11-01 17:23:24 +0330

    Revert "Revert "use sync mode""
    This reverts commit 695c53da99385b269a927bcdee44185f6a278a6b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
-
 import RecordBtn from './components/recordBtn'
-
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const startRecording = () => {
-
-  };
-
   return (
     <div className="App">
       <div className="container">
---
+++ src/utils/index.tsx
@@ -0,0 +1,9 @@
+
+export const blobToBase64 = async (blob:Blob): Promise<unknown> => {
+  return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.readAsDataURL(blob);
+  });
+}
+
